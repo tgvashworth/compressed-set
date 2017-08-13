3bb0e07ec62d73801e6ed489fbbf05c541fe38a7
Tom Ashworth <tashworth@twitter.com>
2017-08-13 22:38:20 +0100

    compressed set

---
+++ CompressedSet.js
@@ -0,0 +1,135 @@
+const MurmurHash3 = require('imurmurhash');
+const URLSafeBase64 = require('urlsafe-base64');
+
+/**
+ * The CompressedSet is a lossy, compressed set data structure inspired by Jeff Hodges'
+ * "The Opposite of a Bloom Filter" article:
+ *     https://www.somethingsimilar.com/2012/05/21/the-opposite-of-a-bloom-filter/
+ *
+ * The idea is that it's useful to have a compressed, probabalistic set data structure
+ * that can answer "probably not in set" with a high degree of confidence (few false positives)
+ * and "probably in set" with a medium degree of confidence (some false negatives)
+ *
+ * It allows, for example, representing cache state over a resource-constrained network.
+ *
+ * Both false negatives and false positives are tunable, compromising on size at higher values.
+ *
+ * The CompressedSet compresses the input by taking part of the hash of the input it to find
+ * an index within a constrained array, and then places some of the double-hashed input
+ * at each index. It ignores collisons.
+ *
+ * Digest is expected a base64 encoded hexidecimal string:
+ * [ 2 bytes for the value count ][ 1 bytes for bytes per value ][ the rest are entries ]
+ */
+
+const Base64 = {
+    encode: s => URLSafeBase64.encode(Buffer.from(s, 'utf8')),
+    decode: s => URLSafeBase64.decode(s).toString('ascii')
+};
+
+class CompressedSet {
+    constructor(buffer) {
+        this.buffer = buffer || new ArrayBuffer(
+            CompressedSet.NUM_VALUES_BYTES +
+            CompressedSet.BYTES_PER_VALUE_BYTES +
+            (CompressedSet.DEFAULT_NUM_VALUES * CompressedSet.DEFAULT_BYTES_PER_VALUE)
+        );
+        this.numValuesView = new DataView(
+            this.buffer,
+            0,
+            CompressedSet.NUM_VALUES_BYTES
+        );
+        this.bytesPerValueView = new DataView(
+            this.buffer,
+            CompressedSet.NUM_VALUES_BYTES,
+            CompressedSet.BYTES_PER_VALUE_BYTES
+        );
+        this.valuesView = new DataView(
+            this.buffer,
+            CompressedSet.NUM_VALUES_BYTES + CompressedSet.BYTES_PER_VALUE_BYTES
+        );
+
+        if (!buffer) {
+            this.numValuesView.setUint16(0, CompressedSet.DEFAULT_NUM_VALUES);
+        }
+        if (!buffer) {
+            this.bytesPerValueView.setUint8(0, CompressedSet.DEFAULT_BYTES_PER_VALUE);
+        }
+
+        this.indexMask = this.numValues - 1;
+        this.valueMask = Math.pow(2, this.bytesPerValue * 8) - 1;
+    }
+
+    get numValues() {
+        return this.numValuesView.getUint16(0);
+    }
+
+    get bytesPerValue() {
+        return this.bytesPerValueView.getUint8(0);
+    }
+
+    toString() {
+        return this.encode();
+    }
+
+    add(entry) {
+        const { k, v } = this._kv(entry);
+
+        this._vs(v).forEach((vByte, i) => {
+            this.valuesView.setUint8(k + i, vByte);
+        });
+
+        return this;
+    }
+
+    contains(entry) {
+        const { k, v } = this._kv(entry);
+        const vs = this._vs(v);
+
+        return this._vs(v).every((vByte, i) =>  this.valuesView.getUint8(k + i) === vByte);
+    }
+
+    _kv(entry) {
+        const hash = CompressedSet.hash(entry);
+        const k = (hash & this.indexMask) * this.bytesPerValue;
+        const v = CompressedSet.hash(hash.toString(16)) & this.valueMask;
+        return { k, v };
+    }
+
+    _vs(v) {
+        const mask = Math.pow(2, 8) - 1;
+        const vs = Array.from({ length: this.bytesPerValue });
+        for (let i = 0; i < this.bytesPerValue; i++) {
+            const shift = this.bytesPerValue - i - 1;
+            vs[i] = (v >> (shift * 8)) & mask;
+        }
+        return vs;
+    }
+
+    encode() {
+        return Base64.encode(Buffer.from(this.buffer).toString('hex'));
+    }
+}
+
+CompressedSet.NUM_VALUES_BYTES = 2;
+CompressedSet.BYTES_PER_VALUE_BYTES = 1;
+CompressedSet.DEFAULT_NUM_VALUES = 256;
+CompressedSet.DEFAULT_BYTES_PER_VALUE = 3;
+
+CompressedSet.decode = function decode(encodedDigest) {
+    const digest = Base64.decode(encodedDigest);
+    const byteLength = digest.length / 2;
+    const arrayBuf = new ArrayBuffer(byteLength);
+    const view = new DataView(arrayBuf);
+    for (let i = 0; i < byteLength; i += 2) {
+        const chars = digest[i] + digest[i + 1];
+        view.setUint8(i, parseInt(chars, 16));
+    }
+    return new CompressedSet(arrayBuf);
+};
+
+CompressedSet.hash = function (v) {
+    return MurmurHash3(v).result();
+};
+
+module.exports = CompressedSet;
---
+++ test.js
@@ -0,0 +1,64 @@
+const random = require('random-js')();
+const bigInt = require('big-integer');
+const leftPad = require('left-pad');
+
+const CompressedSet = require('./CompressedSet');
+
+const source_ids = Array.from({ length: 20000 }).map(() =>
+    bigInt.randBetween('0', '12345678987654321').toString()
+);
+
+const ids = source_ids.slice(0, 200);
+const other_ids_deduped = source_ids.slice(200).filter(i => !ids.includes(i));
+
+//const sizes = [64, 128, 256, 512, 1024, 2048];
+//const value_sizes = [4, 6, 8];
+const results = [];
+
+//for (let s of sizes) {
+    //for (let v of value_sizes) {
+        const hset = new CompressedSet();
+
+        for (let id of ids) {
+            hset.add(id);
+        }
+
+        console.log(hset.encode());
+
+        // sample a random ids from the orignal list for testing
+        const test_ids = random.sample(ids, 100);
+
+        let false_negatives = 0;
+        for (let id of test_ids) {
+            if (!hset.contains(id)) {
+                false_negatives += 1
+            }
+        }
+
+        let false_positives = 0;
+        for (let id of other_ids_deduped) {
+            if (hset.contains(id)) {
+                false_positives += 1
+            }
+        }
+
+        const set_max_size = 512 * 8 / 8;
+
+        results.push(['512', '8', set_max_size, false_negatives, false_positives]);
+    //}
+//}
+
+console.log('configuration,size (bytes),false negatives,false positives');
+results.forEach(([s, v, size, f_n, f_p]) => {
+    console.log(`${s}/${v},${size},${f_n},${f_p}`);
+});
+
+
+const h1 = new CompressedSet();
+h1.add('a');
+console.log(h1.encode());
+console.log(CompressedSet.decode(h1.encode()));
+const h2 = new CompressedSet();
+h2.add('test');
+console.log(CompressedSet.decode(h2.encode()).contains('test') === true);
+console.log(CompressedSet.decode(CompressedSet.decode(h2.encode()).encode()).contains('test') === true);
